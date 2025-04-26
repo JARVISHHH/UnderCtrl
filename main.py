676ef6c77422e3eac2dffde500c96c406e6284d8
Yingtong Yu <942126168@qq.com>
2025-04-26 04:55:22 -0400

    integrate model into main, load needed dataset only

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import argparse
-import tensorflow as tf 
-from cldm import ControlNet, ControlledUnetModel, ControlLDM
-import keras_cv
-from keras_cv.models.stable_diffusion.diffusion_model import DiffusionModel
-from test_imgs import fill50k, facesynthetics
+import tensorflow as tf
+from cldm.cldm import ControlSDB
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train and Test ControlNet")
     parser.add_argument('--mode', choices=['train', 'test'], required=True, help='Mode: train or test')
     parser.add_argument('--dataset', type=str, default='fill50k', choices=['fill50k', 'facesynthetics'])
-    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--epochs', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--save_dir', type=str, default='./checkpoints')
@@ -19,27 +16,27 @@ def main():
     args = parse_args()
 
     if args.dataset == 'fill50k':
+        from test_imgs import fill50k
         dataset = fill50k.get_dataset()
+        dataset_length = 50000
     else:
+        from test_imgs import facesynthetics
         dataset = facesynthetics.get_dataset()
+        dataset_length = 50000  # TODO
 
     # split dataset into train and test
-    train_size = int(len(dataset) * 0.8)
+    train_size = int(dataset_length * 0.8)
     train_dataset = dataset.take(train_size)
     test_dataset = dataset.skip(train_size)
 
-    stable_diffusion = keras_cv.models.StableDiffusion(512, 512)
-    control_net = ControlNet()
-    controlled_unet = ControlledUnetModel()
-    model = ControlLDM(stable_diffusion, control_net, controlled_unet)
+    model = ControlSDB()
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
     model.compile(
         optimizer=optimizer,
-        loss=tf.keras.losses.MeanAbsoluteError(),
+        loss="mse",
     )
-    for _ in range(args.epochs):
-        model.train_step(train_dataset)
+    model.fit(train_dataset, epochs=args.epochs)
     
 if __name__ == '__main__':
     main()
