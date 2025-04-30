d387bcac29d6506c46b09ed6062230721e834032
Q Wang <qingyu_wang@brown.edu>
2025-04-29 21:08:15 -0400

    feat: plot jpg and hint when test data loader

--- test_imgs/test_dataloader.py
+++ test_imgs/test_dataloader.py
@@ -13,12 +13,19 @@ import argparse
 import fill50k
 import facesynthetics
 
+# import ControlSDB from ../cldm/cldm.py
+from cldm.cldm import ControlSDB
+
+import matplotlib.pyplot as plt 
+
 def test_get_dataset():
     # parse command line arguments
     parser = argparse.ArgumentParser(description="Test data loader")
     parser.add_argument('--fill50k', action='store_true', help='Use fill50k dataset')
     parser.add_argument('--facesynthetics', action='store_true', help='Use facesynthetics dataset')
     args = parser.parse_args()
+
+    model = ControlSDB(optimizer=tf.keras.optimizers.Adam(), img_height=256, img_width=256)
 
     # select dataset based on command line argument
     if args.fill50k:
@@ -31,25 +38,37 @@ def test_get_dataset():
         print("Please specify a dataset with --fill50k or --facesynthetics")
         return
 
-    train_dataset, test_dataset = get_dataset(batch_size=8, img_size=256)
+    train_dataset, test_dataset, dataset_length = get_dataset(model, batch_size=1, img_size=256)
 
     for i, data in enumerate(train_dataset):
         print(f"Batch {i}:")
         print(f"jpg shape: {data['jpg'].shape}")
         print(f"hint shape: {data['hint'].shape}")
+
+        # plot the image example
+        plt.imshow(data['jpg'][0])
+        plt.axis('off')
+        plt.title("jpg")
+        plt.show()
+
+        plt.imshow(data['hint'][0])
+        plt.axis('off')
+        plt.title("hint")
+        plt.show()
+        
         print(f"txt: {data['txt']}")
         if i == 1:
             break
     print("Test for train dataset completed.")
     
-    for i, data in enumerate(train_dataset):
-        print(f"Batch {i}:")
-        print(f"jpg shape: {data['jpg'].shape}")
-        print(f"hint shape: {data['hint'].shape}")
-        print(f"txt: {data['txt']}")
-        if i == 1:
-            break
-    print("Test for test dataset completed.")
+    # for i, data in enumerate(test_dataset):
+    #     print(f"Batch {i}:")
+    #     print(f"jpg shape: {data['jpg'].shape}")
+    #     print(f"hint shape: {data['hint'].shape}")
+    #     print(f"txt: {data['txt']}")
+    #     if i == 1:
+    #         break
+    # print("Test for test dataset completed.")
     
 if __name__ == "__main__":
     test_get_dataset()
